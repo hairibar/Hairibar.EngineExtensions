cf5d862d4818dd37dc2c00ab6ec6f0b25d291b54
Aitor Iribar <aitor.iribar@gmail.com>
2019-12-31 12:19:44 +0100

    Added a SceneManager.LoadScene overload that takes an action as a callback

---
+++ Runtime/API Extensions/SceneManagerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine.SceneManagement;
+
+public static class SceneManagerExtensions
+{
+    /// <summary>
+    /// Calls SceneManager.LoadScene(), and calls the given Action as a callback when on ActiveSceneChanged.
+    /// </summary>
+    public static void LoadScene(string scene, LoadSceneMode loadSceneMode, Action onSceneActiveCallback)
+    {
+        SceneManager.LoadScene(scene, loadSceneMode);
+
+        SceneManager.activeSceneChanged += CallbackInvoker;
+
+        void CallbackInvoker(Scene previousScene, Scene newScene)
+        {
+            SceneManager.activeSceneChanged -= CallbackInvoker;
+
+            onSceneActiveCallback();
+        }
+    }
+}
