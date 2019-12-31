5797cd4d0ad10ae2e66a9cd25147e3327998ab32
Aitor Iribar <aitor.iribar@gmail.com>
2019-12-31 12:15:07 +0100

    Added Physics2D.IgnoreCollisions(Rigidbody2D a, Rigidbody2D b, bool ignore)

---
+++ Runtime/API Extensions/Physics2DExtensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public static class Physics2DExtensions
+{
+    public static void IgnoreCollisions(Rigidbody2D a, Rigidbody2D b, bool ignore)
+    {
+        int aColliderCount = a.attachedColliderCount;
+        int bColliderCount = b.attachedColliderCount;
+
+        Collider2D[] aColliders = new Collider2D[aColliderCount];
+        Collider2D[] bColliders = new Collider2D[bColliderCount];
+
+        a.GetAttachedColliders(aColliders);
+        b.GetAttachedColliders(bColliders);
+
+        for (int i = 0; i < aColliderCount; i++)
+        {
+            for (int j = 0; j < bColliderCount; j++)
+            {
+                Physics2D.IgnoreCollision(aColliders[i], bColliders[j], ignore);
+            }
+        }
+    }
+}
