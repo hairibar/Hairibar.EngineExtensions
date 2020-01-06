2054b8ef91155208843cc37d3b425d42881d677f
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-06 12:46:55 +0100

    Documented RemoveInBuildsAttribute and RemoveInReleaseAttribute.

--- Runtime/Build System/RemoveInBuildsAttribute.cs
+++ Runtime/Build System/RemoveInBuildsAttribute.cs
@@ -2,6 +2,10 @@
 
 namespace Hairibar.EngineExtensions
 {
+    /// <summary>
+    /// Any component with this attribute will be removed at build time.
+    /// Components must not have any extra serialized properties when UNITY_EDITOR is enabled. 
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class RemoveInBuildsAttribute : Attribute
     {
