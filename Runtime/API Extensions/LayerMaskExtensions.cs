6dbbbe736d5c8b020697a4dd85a5c7dea7b9c7b3
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-06 12:32:05 +0100

    Documented LayerMaskExtensions.

--- Runtime/API Extensions/LayerMaskExtensions.cs
+++ Runtime/API Extensions/LayerMaskExtensions.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Hairibar.EngineExtensions
 {
     public static class LayerMaskExtensions
     {
+        /// <summary>
+        /// Checks whether the given layer is enabled in the given LayerMask.
+        /// </summary>
         public static bool LayerIsEnabled(this LayerMask mask, int layer)
         {
             return mask == (mask | (1 << layer));
         }
     }
-
 }
