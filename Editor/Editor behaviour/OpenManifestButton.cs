cdbcbb1a2da0a4b81a33a8c07482f05e14f73ace
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-09 10:25:08 +0100

    Added a menu item that opens the package manifest. Closes #2.

---
+++ Editor/Editor behaviour/OpenManifestButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Hairibar.EngineExtensions.Editor
+{
+    /// <summary>
+    /// Adds a menu item that opens the package manifest.
+    /// </summary>
+    static class OpenManifestButton
+    {
+        const string MANIFEST_PATH = "Packages/manifest.json";
+
+        [MenuItem("Window/Package Manifest", priority = 1501)]
+        static void OpenPackageManifest()
+        {
+            EditorUtility.OpenWithDefaultApp(MANIFEST_PATH);
+        }
+    }
+}
