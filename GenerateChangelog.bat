dbec1a81553876795d25dd2815aaefefa9e782fc
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-09 09:36:53 +0100

    Removed obsolote GenerateChangelog.bat.

--- GenerateChangelog.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-auto-changelog --output "CHANGELOG.md" ^
---template compact --commit-limit false -backfill-limit false ^
---breaking-pattern "\[BREAKING\]" --ignore-commit-pattern "(version number)" ^
---include-branch "upm" --sort-commits "date" ^
-& ^
-start CHANGELOG.md
