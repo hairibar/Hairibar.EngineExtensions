de2f9462d6abbd4fd6d4b4490c0e6f47f8728ab7
Aitor Iribar <aitor.iribar@gmail.com>
2020-07-15 18:06:29 +0200

    Added RaycastHit2D.StartedAtCollider()

---
+++ Runtime/API Extensions/RaycastHit2DExtensions.cs
@@ -0,0 +1,14 @@
+﻿#if PHYSICS_2D_MODULE_PRESENT
+using UnityEngine;
+
+namespace Hairibar.EngineExtensions
+{
+    public static class RaycastHit2DExtensions
+    {
+        public static bool StartedAtCollider(this RaycastHit2D hit)
+        {
+            return hit && hit.fraction == 0;
+        }
+    }
+}
+#endif
