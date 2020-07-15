e51be1318760c816e2271fd6cfa1cf8efb346246
Aitor Iribar <aitor.iribar@gmail.com>
2020-07-15 18:06:08 +0200

    Added Component.TryGetComponentInParent

---
+++ Runtime/API Extensions/ComponentExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+#pragma warning disable UNT0014 // Invalid type for call to GetComponent
+namespace Hairibar.EngineExtensions
+{
+    public static class ComponentExtensions
+    {
+        public static bool TryGetComponentInParent<T>(this Component thisComponent, out T result)
+        {
+            result = thisComponent.GetComponentInParent<T>();
+            return result != null;
+        }
+    }
+}
