20802ca63f0772f1219cdb0717dbd684a45a41e8
Aitor Iribar <aitor.iribar@gmail.com>
2020-07-15 19:22:30 +0200

    Added custom drawer for Countdowns

---
+++ Editor/Drawers/CountdownDrawer.cs
@@ -0,0 +1,18 @@
+﻿using NaughtyAttributes.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Hairibar.EngineExtensions.Editor
+{
+    [CustomPropertyDrawer(typeof(Countdown), true)]
+    public class CountdownDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            TooltipAttribute tooltip = PropertyUtility.GetAttribute<TooltipAttribute>(property);
+            label.tooltip = tooltip?.tooltip;
+
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("_length"), label);
+        }
+    }
+}
