90c4d35ec3e6797f05d8f7258d46a8184437a418
Aitor Iribar <aitor.iribar@gmail.com>
2020-07-15 18:07:05 +0200

    Added CopyPosition

---
+++ Runtime/Extra Functionality/CopyPosition.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Hairibar.EngineExtensions
+{
+    public class CopyPosition : MonoBehaviour
+    {
+        public Transform TransformToCopy
+        {
+            get => _trasformToCopy;
+            set
+            {
+                _trasformToCopy = value;
+                enabled = true;
+            }
+        }
+        Transform _trasformToCopy;
+
+        void LateUpdate()
+        {
+            if (TransformToCopy)
+            {
+                transform.position = TransformToCopy.position;
+            }
+            else
+            {
+                enabled = false;
+            }
+        }
+    }
+}
