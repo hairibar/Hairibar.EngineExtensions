af00a3e74796fcb8e76d00070630cac4579adc1f
Aitor Iribar <aitor.iribar@gmail.com>
2020-07-15 18:05:50 +0200

    Added 2D ConeCast

--- Runtime/API Extensions/Physics2DExtensions.cs
+++ Runtime/API Extensions/Physics2DExtensions.cs
@@ -27,6 +27,46 @@ namespace Hairibar.EngineExtensions
                 }
             }
         }
+
+        public static int ConeCast(Vector2 origin, Vector2 centerDirection, float angle, float distance, int rayCount, LayerMask layerMask, RaycastHit2D[] hits, bool debugRays = false)
+        {
+            int nextHitIndex = 0;
+
+            float startAngle = -angle / 2 * Mathf.Deg2Rad;
+            float endAngle = angle / 2 * Mathf.Deg2Rad;
+
+            for (int rayIndex = 0; rayIndex < rayCount; rayIndex++)
+            {
+                if (nextHitIndex >= hits.Length) return nextHitIndex - 1;
+
+                Vector2 rayDirection = GetRayDirection(rayIndex);
+                RaycastHit2D hit = Physics2D.Raycast(origin, rayDirection, distance, layerMask);
+
+                if (hit)
+                {
+                    hits[nextHitIndex] = hit;
+                    nextHitIndex++;
+                }
+
+                if (debugRays)
+                {
+                    Debug.DrawRay(origin, rayDirection * (hit ? hit.distance : distance),
+                        hit ? Color.red : Color.white, 1);
+                }
+            }
+
+            return nextHitIndex - 1;
+
+
+            Vector2 GetRayDirection(int rayIndex)
+            {
+                float t = rayIndex / (float) rayCount;
+                float rayAngle = Mathf.Lerp(startAngle, endAngle, t);
+
+                Vector2 rayDirection = Vector2Extensions.Rotate(centerDirection, rayAngle);
+                return rayDirection;
+            }
+        }
     }
 }
 #endif
