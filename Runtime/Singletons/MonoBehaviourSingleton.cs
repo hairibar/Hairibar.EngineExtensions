21789c552477ecf7974ef7f952b726b479adf6db
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-22 18:07:21 +0100

    [FEAT] Added MonoBehaviourSingleton. Use with care, do not overuse.

---
+++ Runtime/Singletons/MonoBehaviourSingleton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Hairibar.EngineExtensions
+{
+    /// <summary>
+    /// Do not overuse.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviourSingleton<T>
+    {
+        public static T Instance
+        {
+            get
+            {
+                CreateIfNecessary();
+                return _instance;
+            }
+        }
+        static T _instance;
+
+        static void CreateIfNecessary()
+        {
+            if (!_instance)
+            {
+                _instance = FindObjectOfType<T>();
+            }
+
+            if (!_instance)
+            {
+                GameObject go = new GameObject(typeof(T).Name);
+                _instance = go.AddComponent<T>();
+            }
+        }
+    }
+}
