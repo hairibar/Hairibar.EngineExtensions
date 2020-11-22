9d4f4376264966ce8c373c023461d2ffad66d8a8
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-22 12:40:34 +0100

    [FEAT] Added Animator.IsPlayingAnimation() and Animator.AnimationIsFinished()

---
+++ Runtime/API Extensions/AnimatorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Hairibar.EngineExtensions
+{
+    public static class AnimatorExtensions
+    {
+        public static bool IsPlayingAnimation(this Animator animator, string animationName, int layer = 0)
+        {
+            return animator.GetCurrentAnimatorStateInfo(layer).IsName(animationName);
+        }
+
+        public static bool AnimationIsFinished(this Animator animator, int layer = 0)
+        {
+            return animator.GetCurrentAnimatorStateInfo(layer).normalizedTime >= 1;
+        }
+
+        public static bool AnimationIsFinished(this Animator animator, string animationName, int layer = 0)
+        {
+            return animator.IsPlayingAnimation(animationName, layer) && animator.AnimationIsFinished(layer);
+        }
+    }
+}
