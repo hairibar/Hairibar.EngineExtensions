d14d16a676f9416c03add39a2a3c8b331fc0919e
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-22 12:40:01 +0100

    [FEAT] Added GameObject.TryGetComponentInChildren() and GameObject.TryGetComponentInParent()

---
+++ Runtime/API Extensions/GameObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Hairibar.EngineExtensions
+{
+    public static class GameObjectExtensions
+    {
+        public static bool TryGetComponentInParent<T>(this GameObject gameObject, out T result)
+        {
+            result = gameObject.GetComponentInParent<T>();
+            return result != null;
+        }
+
+        public static bool TryGetComponentInChildren<T>(this GameObject gameObject, out T component, bool includeInactive = false)
+        {
+            component = gameObject.GetComponentInChildren<T>(includeInactive);
+            return component != null;
+        }
+    }
+}
