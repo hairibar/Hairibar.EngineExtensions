47aa4aef1a97dbefb398c6ad86a0e7644b89e825
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-22 17:55:39 +0100

    [FEAT] Added RendererUtilities.GetTotalBounds()

---
+++ Runtime/Utilities/RendererUtilities.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hairibar.EngineExtensions
+{
+    public static class RendererUtilities
+    {
+        public static Bounds GetTotalBounds(IReadOnlyCollection<Renderer> renderers)
+        {
+            Bounds totalBounds = new Bounds();
+            bool isFirstRenderer = true;
+
+            foreach (Renderer renderer in renderers)
+            {
+                if (isFirstRenderer)
+                {
+                    totalBounds = renderer.bounds;
+                    isFirstRenderer = false;
+                }
+                else
+                {
+                    totalBounds.Encapsulate(renderer.bounds);
+                }
+            }
+
+            return totalBounds;
+        }
+    }
+}
