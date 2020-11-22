415acb4f1751f872e6c96d355a0814ef9c54ba30
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-22 18:18:03 +0100

    [FEAT] Added VersionNumberText component

---
+++ Runtime/Debug Tools/VersionNumberText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Hairibar.EngineExtensions
+{
+    [RequireComponent(typeof(Text))]
+    public class VersionNumberText : MonoBehaviour
+    {
+        void Start()
+        {
+            if (!Application.isEditor && Debug.isDebugBuild)
+            {
+                GetComponent<Text>().text = Application.version;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
