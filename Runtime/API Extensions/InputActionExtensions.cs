7a037b2544b7baf35470ff8aaa2750e251fc1da1
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-22 13:25:18 +0100

    [FEAT] Added InputSystem.InputAction.IsPressed()

---
+++ Runtime/API Extensions/InputActionExtensions.cs
@@ -0,0 +1,16 @@
+﻿# if INPUT_SYSTEM_PRESENT
+
+using UnityEngine.InputSystem;
+
+namespace Hairibar.EngineExtensions
+{
+    public static class InputActionExtensions
+    {
+        public static bool IsPressed(this InputAction action, float threshold = 0.5f)
+        {
+            return action.ReadValue<float>() > threshold;
+        }
+    }
+}
+
+#endif
