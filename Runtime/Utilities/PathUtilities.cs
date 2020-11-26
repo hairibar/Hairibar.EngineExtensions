4b28619b16c72c2a1d2ac2220fae2bb80643086e
Aitor Iribar <aitor.iribar@gmail.com>
2020-11-26 13:36:44 +0100

    [FEAT] Added PathUtilities.GetRelativePath() and PathUtilities.AppendDirectorySeparatorChar()

---
+++ Runtime/Utilities/PathUtilities.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace Hairibar.EngineExtensions
+{
+    public static class PathUtilities
+    {
+        /// <summary>
+        /// Creates a relative path from one file or folder to another.
+        /// </summary>
+        /// <param name="fromPath">Contains the directory that defines the start of the relative path.</param>
+        /// <param name="toPath">Contains the path that defines the endpoint of the relative path.</param>
+        /// <returns>The relative path from the start directory to the end path.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="fromPath"/> or <paramref name="toPath"/> is <c>null</c>.</exception>
+        /// <exception cref="UriFormatException"></exception>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static string GetRelativePath(string fromPath, string toPath)
+        {
+            //Originally from https://stackoverflow.com/a/340454
+            if (string.IsNullOrEmpty(fromPath))
+            {
+                throw new ArgumentNullException(nameof(fromPath));
+            }
+
+            if (string.IsNullOrEmpty(toPath))
+            {
+                throw new ArgumentNullException(nameof(toPath));
+            }
+
+            Uri fromUri = new Uri(AppendDirectorySeparatorChar(fromPath));
+            Uri toUri = new Uri(AppendDirectorySeparatorChar(toPath));
+
+            if (fromUri.Scheme != toUri.Scheme)
+            {
+                return toPath;
+            }
+
+            Uri relativeUri = fromUri.MakeRelativeUri(toUri);
+            string relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+            if (string.Equals(toUri.Scheme, Uri.UriSchemeFile, StringComparison.OrdinalIgnoreCase))
+            {
+                relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+            }
+
+            return relativePath;
+
+
+
+        }
+
+        public static string RelativeTo(this DirectoryInfo directory, string relativeTo)
+        {
+            return GetRelativePath(relativeTo, directory.FullName);
+        }
+
+
+        /// <summary>
+        /// Append a slash only if the path is a directory and does not have a slash.
+        /// </summary>
+        /// <returns></returns>
+        public static string AppendDirectorySeparatorChar(string path)
+        {
+            if (!Path.HasExtension(path) &&
+                !path.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                return path + Path.DirectorySeparatorChar;
+            }
+
+            return path;
+        }
+    }
+}
