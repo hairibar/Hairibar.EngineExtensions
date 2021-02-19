d14d0c52d14642f91c5da37c1fdee4ddef3331e6
Aitor Iribar <aitor.iribar@gmail.com>
2021-02-19 12:10:15 +0100

    [FEAT] Made CollisionEventDispatcher pass itself for easy identification

--- Runtime/Extra Functionality/Components/CollisionEventDispatcher.cs
+++ Runtime/Extra Functionality/Components/CollisionEventDispatcher.cs
@@ -6,87 +6,87 @@ namespace Hairibar.EngineExtensions
     public class CollisionEventDispatcher : MonoBehaviour
     {
 #if PHYSICS_MODULE_PRESENT
-        public event System.Action<Collision> OnCollisionEntered;
-        public event System.Action<Collision> OnCollisionStayed;
-        public event System.Action<Collision> OnCollisionExited;
+        public event System.Action<Collision, CollisionEventDispatcher> OnCollisionEntered;
+        public event System.Action<Collision, CollisionEventDispatcher> OnCollisionStayed;
+        public event System.Action<Collision, CollisionEventDispatcher> OnCollisionExited;
 
-        public event System.Action<Collider> OnTriggerEntered;
-        public event System.Action<Collider> OnTriggerStayed;
-        public event System.Action<Collider> OnTriggerExited;
+        public event System.Action<Collider, CollisionEventDispatcher> OnTriggerEntered;
+        public event System.Action<Collider, CollisionEventDispatcher> OnTriggerStayed;
+        public event System.Action<Collider, CollisionEventDispatcher> OnTriggerExited;
 #endif
 
 #if PHYSICS_2D_MODULE_PRESENT
-        public event System.Action<Collision2D> OnCollisionEntered2D;
-        public event System.Action<Collision2D> OnCollisionStayed2D;
-        public event System.Action<Collision2D> OnCollisionExited2D;
+        public event System.Action<Collision2D, CollisionEventDispatcher> OnCollisionEntered2D;
+        public event System.Action<Collision2D, CollisionEventDispatcher> OnCollisionStayed2D;
+        public event System.Action<Collision2D, CollisionEventDispatcher> OnCollisionExited2D;
 
-        public event System.Action<Collider2D> OnTriggerEntered2D;
-        public event System.Action<Collider2D> OnTriggerStayed2D;
-        public event System.Action<Collider2D> OnTriggerExited2D;
+        public event System.Action<Collider2D, CollisionEventDispatcher> OnTriggerEntered2D;
+        public event System.Action<Collider2D, CollisionEventDispatcher> OnTriggerStayed2D;
+        public event System.Action<Collider2D, CollisionEventDispatcher> OnTriggerExited2D;
 #endif
 
 #if PHYSICS_MODULE_PRESENT
         void OnCollisionEnter(Collision collision)
         {
-            OnCollisionEntered?.Invoke(collision);
+            OnCollisionEntered?.Invoke(collision, this);
         }
 
         void OnCollisionExit(Collision collision)
         {
-            OnCollisionExited?.Invoke(collision);
+            OnCollisionExited?.Invoke(collision, this);
         }
 
         void OnCollisionStay(Collision collision)
         {
-            OnCollisionStayed?.Invoke(collision);
+            OnCollisionStayed?.Invoke(collision, this);
         }
 
         void OnTriggerEnter(Collider other)
         {
-            OnTriggerEntered?.Invoke(other);
+            OnTriggerEntered?.Invoke(other, this);
         }
 
         void OnTriggerStay(Collider other)
         {
-            OnTriggerStayed?.Invoke(other);
+            OnTriggerStayed?.Invoke(other, this);
         }
 
         void OnTriggerExit(Collider other)
         {
-            OnTriggerExited?.Invoke(other);
+            OnTriggerExited?.Invoke(other, this);
         }
 #endif
 
 #if PHYSICS_2D_MODULE_PRESENT
         void OnCollisionEnter2D(Collision2D collision)
         {
-            OnCollisionEntered2D?.Invoke(collision);
+            OnCollisionEntered2D?.Invoke(collision, this);
         }
 
         void OnCollisionStay2D(Collision2D collision)
         {
-            OnCollisionStayed2D?.Invoke(collision);
+            OnCollisionStayed2D?.Invoke(collision, this);
         }
 
         void OnCollisionExit2D(Collision2D collision)
         {
-            OnCollisionExited2D?.Invoke(collision);
+            OnCollisionExited2D?.Invoke(collision, this);
         }
 
 
         void OnTriggerEnter2D(Collider2D other)
         {
-            OnTriggerEntered2D?.Invoke(other);
+            OnTriggerEntered2D?.Invoke(other, this);
         }
 
         void OnTriggerStay2D(Collider2D other)
         {
-            OnTriggerStayed2D?.Invoke(other);
+            OnTriggerStayed2D?.Invoke(other, this);
         }
 
         void OnTriggerExit2D(Collider2D other)
         {
-            OnTriggerExited2D?.Invoke(other);
+            OnTriggerExited2D?.Invoke(other, this);
         }
 #endif
     }
